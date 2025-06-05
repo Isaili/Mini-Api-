d7ba42fb81f34b2c85bb977d360f860f52660bab
Isai <231173@ids.upchiapas.edu.mx>
2025-06-05 10:24:12 -0600

    ya no verifico el token

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,11 +2,11 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   const token = req.header("Authorization")?.split(" ")[1];
-  if (!token) return res.status(401).json({ msg: "Token requerido" });
+  // if (!token) return res.status(401).json({ msg: "Token requerido" });
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user;
+    // const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // req.user = decoded.user;
     next();
   } catch {
     res.status(401).json({ msg: "Token inválido" });
